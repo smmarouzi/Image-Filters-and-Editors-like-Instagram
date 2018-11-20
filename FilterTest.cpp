108b60e1ba8ef7ea53eda350871d55b16d330de9
Samaneh Marouzi <sm.marouzi@gmail.com>
2018-11-20 13:58:52 -0500

    Add files via upload

---
+++ FilterTest.cpp
@@ -0,0 +1,22 @@
+#include "..\include\GloServise.h"
+
+/*
+int main()
+{
+	GloService gs;
+
+	gs.gamma("../GLoFilters/data/giftbag29.jpg", 0.7, "../GLoFilters/result2");
+	gs.sepia("../GLoFilters/data/giftbag29.jpg", 0.7, "../GLoFilters/result2");
+	//gs.sepiatone("../GLoFilters/data/giftbag29.jpg", 0.7, "../GLoFilters/result2");
+	gs.hue_rotate("../GLoFilters/data/giftbag29.jpg", -20, "../GLoFilters/result2");
+	gs.saturate("../GLoFilters/data/giftbag29.jpg", 1.5, "../GLoFilters/result2");
+	gs.bright("../GLoFilters/data/giftbag29.jpg", 1.5, "../GLoFilters/result2");
+	gs.contrast("../GLoFilters/data/giftbag29.jpg", 1.5, "../GLoFilters/result2");
+	gs.gray("../GLoFilters/data/giftbag29.jpg",1.5, "../GLoFilters/result2");
+	gs.xproii("../GLoFilters/data/giftbag29.jpg", 0.2, "../GLoFilters/result2");
+	gs.hudson("../GLoFilters/data/giftbag29.jpg", 0.4, "../GLoFilters/result2");
+	gs.juno("../GLoFilters/data/giftbag29.jpg", 0.4, "../GLoFilters/result2");
+	gs.ludwig("../GLoFilters/data/giftbag29.jpg", 0.2, "../GLoFilters/result2");
+
+	return 0;
+}*/
